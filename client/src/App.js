acfa846cf6019a044d8d10c1e645a0361b27012b
varshilgadhiya <varshilgadhiya5784@gmail.com>
2022-02-16 16:12:33 +0530

    add  profile

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,25 @@ import Registrationform from './component/Registrationform';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Loginform from './component/Loginform'
 import Navigationbar from './component/user/Navigationbar';
-import Profile from './component/user/Profile';
 import Logout from './component/Logout';
+import Profile from './component/user/Profile';
 
 function App() {
   return (
     <>
       {
         localStorage.getItem("user") ?
-        //auth route
+          //auth route
           <>
             {
               localStorage.getItem("role") === "admin" ?
-              //admin route
+                //admin route
                 <BrowserRouter>
                   <Navigationbar />
                   <Routes>
-                    <Route exact path="/profile" element={<Profile />} />
                     <Route exact path="/logout" element={<Logout />} />
+                    <Route exact path="/profile" element={<Profile />} />
+
                   </Routes>
                 </BrowserRouter>
                 :
--- client/src/component/user/Profile.jsx
+++ client/src/component/user/Profile.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import { ListGroup, ListGroupItem, Card } from 'react-bootstrap'
+import React from 'react'
 
 const Profile = () => {
-  return (
-    <div>{localStorage.getItem("role") === "admin" ? "admin" : "user"}</div>
-  );
-};
+    return (
+        <div className='row'><Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+                <Card.Title>E-COMMERCE</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                <ListGroupItem>Cras justo odio</ListGroupItem>
+                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                <ListGroupItem>Vestibulum at eros</ListGroupItem>
+            </ListGroup>
 
-export default Profile;
+        </Card></div>
+    )
+}
+
+export default Profile
