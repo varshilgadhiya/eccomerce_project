a746966fb93671944a67a1af631e370bbc039742
Gohil Harsh <66783334+harsh1110@users.noreply.github.com>
2022-02-20 23:57:24 +0530

    add shopping card

--- client/src/component/user/Shopping.jsx
+++ client/src/component/user/Shopping.jsx
@@ -1,19 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import CheckCircleOutlineSharpIcon from '@material-ui/icons/CheckCircleOutlineSharp';
-import ControlPointSharpIcon from '@material-ui/icons/ControlPointSharp';
-import ErrorOutlineSharpIcon from '@material-ui/icons/ErrorOutlineSharp';
-import NotInterestedSharpIcon from '@material-ui/icons/NotInterestedSharp';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Shopping = ({ product }) => {
-    const navigate = useNavigate()
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        axios.get(`http://localhost:5000/product/`, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } })
+        axios.get(`http://localhost:5000/product/`, { headers: { "Authorization": `Bearer ${localStorage.getItem("token")}` } })
             .then((value) => {
                 setProducts(value.data)
-                navigate = "/"
+                console.log(products);
             })
     }, [])
 
@@ -24,7 +20,42 @@ const Shopping = ({ product }) => {
         <div className="my-4">
             <h4 className="text-center state">All Products</h4>
             <hr />
-            
+            <Grid container className='px-4' spacing={2}>
+                {products !== undefined?
+                    products.map((product, index) => (
+                        <Grid item sm={3} xs={12}>
+                            <Card className='p-card' sx={{ maxWidth: 345 }}>
+                                <CardMedia
+                                    component="img"
+                                    height="200"
+                                    image={product.pic?product.pic[0]:null}
+                                    alt={product.name}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {product.name}
+                                    </Typography>
+                                    <Typography gutterBottom variant="h6" component="div">
+                                        Rs.{product.price} <span class="badge bg-warning text-dark">{product.offer} %</span>
+                                    </Typography>
+                                    <Typography gutterBottom variant="body1" component="div">
+                                        {product.category}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {product.discription}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button className='my-4 mx-3' variant='contained' size="small"><ShoppingCartIcon/>&nbsp; Add To Cart</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))
+                    :
+                    null
+                }
+            </Grid>
+
         </div>
 
     )
