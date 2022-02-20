568787dc6ad63b7ab0b927239287f47e9dffac51
Gohil Harsh <66783334+harsh1110@users.noreply.github.com>
2022-02-21 01:11:01 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import AlluserTable from './component/admin/AlluserTable';
 import Addproduct from "./component/product/Addproduct"
 import Producttable from './component/product/Productable';
 import Upadateproduct from "./component/product/Upadateproduct"
+import Shopping from './component/user/Shopping';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route exact path="/all-user" element={<AlluserTable />} />
           <Route exact path="/all-product" element={<Producttable />} />
           <Route exact path="/add-product" element={<Addproduct />} />
+          <Route exact path="/shopping" element={<Shopping />} />
           <Route exact path="/upadate-product/:id" element={<Upadateproduct />} />
           <Route exact path="/logout" element={<Logout />} />
           <Route exact path="/registration" element={<Registrationform />} />
