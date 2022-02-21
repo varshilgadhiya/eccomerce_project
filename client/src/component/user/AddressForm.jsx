e85fcb8ce6bea57f4866b173a9c6c67c8ee09fc5
varshilgadhiya <varshilgadhiya5784@gmail.com>
2022-02-22 01:07:17 +0530

    Update AddressForm.jsx

--- client/src/component/user/AddressForm.jsx
+++ client/src/component/user/AddressForm.jsx
@@ -2,9 +2,32 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function AddressForm() {
 
+  const [fullname, setFullname] = React.useState('')
+  const [address, setAddress] = useState('')
+  const [city, setCity] = useState('')
+  const [state, setState] = useState('')
+  const [zipcode, setZipcode] = useState('')
+
+ useEffect(() => {
+   
+  axios.post("http://localhost:5000/order/add",)
+  .then((res,req)=>{
+    alert("order placed successfully")
+  })
+  .catch((err)=>{
+    console.log("order placed unsuccessfully")
+  })
+
+ }, [])
+ 
+ 
+   
+  
 
   return (
     <React.Fragment>
@@ -12,46 +35,28 @@ export default function AddressForm() {
         Shipping Address
       </Typography>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={12}>
           <TextField
             required
-            id="firstName"
+            id="FullName"
             name="firstName"
-            label="First name"
+            onChange={(e) => setFullname(e.target.value)}
+            label="Full name"
             fullWidth
             autoComplete="given-name"
             variant="standard"
           />
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="lastName"
-            name="lastName"
-            label="Last name"
-            fullWidth
-            autoComplete="family-name"
-            variant="standard"
-          />
-        </Grid>
         <Grid item xs={12}>
           <TextField
             required
-            id="address1"
-            name="address1"
-            label="Address line 1"
+            id="address"
+            name="address"
+            onChange={(e) => setAddress(e.target.value)}
+
+            label="Address"
             fullWidth
             autoComplete="shipping address-line1"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="address2"
-            name="address2"
-            label="Address line 2"
-            fullWidth
-            autoComplete="shipping address-line2"
             variant="standard"
           />
         </Grid>
@@ -61,6 +66,8 @@ export default function AddressForm() {
             id="city"
             name="city"
             label="City"
+            onChange={(e) => setCity(e.target.value)}
+
             fullWidth
             autoComplete="shipping address-level2"
             variant="standard"
@@ -70,17 +77,21 @@ export default function AddressForm() {
           <TextField
             id="state"
             name="state"
-            label="State/Province/Region"
+            onChange={(e) => setState(e.target.value)}
+
+            label="State"
             fullWidth
             variant="standard"
           />
         </Grid>
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={12}>
           <TextField
             required
             id="zip"
             name="zip"
-            label="Zip / Postal code"
+            onChange={(e) => setZipcode(e.target.value)}
+
+            label="Zip code"
             fullWidth
             autoComplete="shipping postal-code"
             variant="standard"
